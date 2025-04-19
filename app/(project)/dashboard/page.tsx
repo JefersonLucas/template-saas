bde34fa7b442da0d292e73a4f9a3c58a599cfd24
Jeferson Lucas <jeferson.luckas@gmail.com>
2025-04-19 11:13:25 -0300

    feat: google auth

--- app/(project)/dashboard/page.tsx
+++ app/(project)/dashboard/page.tsx
@@ -1,7 +1,34 @@
-export default function Dashboard() {
+"use server"
+
+import { handleAuth } from "@/app/actions/handle-auth"
+import { auth } from "@/app/lib/auth"
+import { redirect } from "next/navigation"
+
+export default async function Dashboard() {
+	// Estamos no lado do servidor
+	const session = await auth()
+	console.log(session)
+
+	if (!session) {
+		redirect("/login")
+	}
+
 	return (
-		<div className="flex flex-col items-center justify-center h-screen">
+		<div className="flex flex-col gap-10 items-center justify-center h-screen">
 			<h1 className="text-4xl font-bold">Protected Dashboard</h1>
+			<p>Name: {session?.user?.name}</p>
+			<p>Email: {session?.user?.email}</p>
+
+			{session.user?.email && (
+				<form action={handleAuth}>
+					<button
+						type="submit"
+						className="border rounded-md px-2 cursor-pointer"
+					>
+						Logout
+					</button>
+				</form>
+			)}
 		</div>
 	)
 }
--- app/(project)/page.tsx
+++ app/(project)/page.tsx
@@ -4,7 +4,9 @@ export default function Home() {
 	return (
 		<div className="flex flex-col items-center justify-center h-screen">
 			<h1 className="text-4xl font-bold">Landing Page</h1>
-			<Link href="/login">Login</Link>
+			<Link href="/login">
+				<button>Login</button>
+			</Link>
 		</div>
 	)
 }
